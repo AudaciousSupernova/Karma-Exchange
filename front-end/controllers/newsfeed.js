5d4272fac02ad0c25f1ff06f20da6e099f6c33d3
Neeraj Kohirkar <neerajk1208@gmail.com>
2016-01-23 14:25:22 -0800

    removed dummy data from front end controllers

--- front-end/controllers/newsfeed.js
+++ front-end/controllers/newsfeed.js
@@ -1,50 +1,7 @@
 angular.module('app.newsfeed', [])
 
 .controller('NewsfeedController', function($scope, $location, Newsfeed) {
-  $scope.trending = [
-    {
-      name: 'Spencer', 
-      scores: [
-        {
-          name: 'Spencer', 
-          score: 98, 
-          date: 'January 1 2013'
-        }, 
-        {
-          name: 'Spencer', 
-          score: 99, 
-          date: 'February 22, 2014'
-        }, 
-        {
-          name: 'Spencer', 
-          score: 22, 
-          date: 'June 2, 2010'
-        }
-      ], 
-      currentScore: 99
-    }, 
-    {
-      name: 'Tessa', 
-      scores: [
-        {
-          name: 'Tessa', 
-          score: 98, 
-          date: 'January 1 2013'
-        }, 
-        {
-          name: 'Tessa', 
-          score: 99, 
-          date: 'February 22, 2014'
-        }, 
-        {
-          name: 'Tessa', 
-          score: 22, 
-          date: 'June 2, 2010'
-        }
-      ], 
-      currentScore: 99
-    }
-  ];
+  $scope.trending;
 
   $scope.getTrending = function() {
     Newsfeed.getTrending()
