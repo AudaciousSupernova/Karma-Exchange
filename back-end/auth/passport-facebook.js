a49f598a3a5c9de29b5d761f3d49b80d872622f3
Ranjit Rao <ranjiterao@gmail.com>
2016-01-22 17:47:29 -0800

    Build out file structure for FB OAuth

---
+++ back-end/auth/passport-facebook.js
@@ -0,0 +1,30 @@
+var passport = require('passport');
+// var constants = require('./../../constants'); 
+var FacebookStrategy = require('passport-facebook').Strategy;
+// var usersController = require('./../components/users/usersController');
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.use(new FacebookStrategy({
+    clientID: 767594746706952,
+    clientSecret: d917065bc815ddf8ab8779c9f0b3c664,
+    callbackURL: constants.API_URL + "/auth/facebook/callback",
+    enableProof: true,
+    //fields from facebook profile that Nova uses; don't need at the moment 
+    // profileFields: ['id', 'name','picture.type(large)', 'emails', 'displayName', 'about', 'gender']
+  },
+  function(accessToken, refreshToken, profile, done) {
+    process.nextTick(function () {
+      console.log('Facebook Profile',profile.id);
+      console.log('Access Token', accessToken);
+    });
+  }
+));
+
+module.exports = passport;`
