a4ae775d11a4da9df9670ec106ba9cc65072250a
Ranjit Rao <ranjiterao@gmail.com>
2016-01-25 14:35:52 -0800

    add index.js to spec folder

--- back-end/spec/index.js
+++ back-end/spec/index.js
@@ -4,11 +4,5 @@ var sinon = require('sinon'):
 // var oAuth = require('../Auth/passport.facebook.js');
 var db = require('../db/index.js'); 
 
-describe("REST-API routes", function() {
 
-	before(function() {
-		// stub will bypass oAuth middleware
-		var ensureAuthenticatedSpy = sinon.stub(oAuth, 'ensureAuth');
-	})
-})
 
