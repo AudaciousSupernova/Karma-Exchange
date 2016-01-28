a69e53e934ce2af49d869904344ed26830f6be10
Ranjit Rao <ranjiterao@gmail.com>
2016-01-27 19:37:58 -0800

    created gulp karma build, angular dependencies

--- karma.conf.js
+++ karma.conf.js
@@ -10,76 +10,77 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'chai', 'sinon', 'browserify'],
+    frameworks: ['mocha', 'chai', 'sinon'],
 
-    plugins: [  
-      "karma-browserify",
-      "karma-mocha",
-      "karma-chai",
-      "karma-sinon",
-      "karma-chrome-launcher",
-      "karma-phantomjs-launcher"
-    ],
+    // plugins: [  
+    //   "karma-browserify",
+    //   "karma-mocha",
+    //   "karma-chai",
+    //   "karma-sinon",
+    //   "karma-chrome-launcher",
+    //   "karma-phantomjs-launcher"
+    // ],
 
     // list of files / patterns to load in the browser
     files: [
       // angular source
       'front-end/libs/angular/angular.js',
       'front-end/libs/angular-route/angular-route.js',
+      'front-end/libs/angular-mocks/angular-mocks.js',
+      'front-end/libs/angular-ui-router/release/angular-ui-router.js',
+      'front-end/libs/angular-material/angular-material.js',
+      'front-end/libs/angular-animate/angular-animate.js',
+      'front-end/libs/angular-aria/angular-aria.js',
 
       // our app code
-      'front-end/**/*.js',
+      'front-end/app/**/*.js',
 
       // our spec files
-      'node_modules/expect.js/index.js',
       'specs/client/routingSpec.js'
     ],
 
-
     // list of files to exclude
-    exclude: [
-      'front-end/libs/angular-animate/*.js',
-      'front-end/libs/angular-aria/*.js'
-    ],
+    // exclude: [
+    //   'front-end/libs/angular-animate/*.js',
+    //   'front-end/libs/angular-aria/*.js',
+    //   'front-end/libs/angular-material',
+    //   'front-end/libs/angular-messages/*.js',
+    //   'front-end/libs/angular-ui-router/*.js'
+    // ],
 
+    // test results reporter to use
+    reporters: ['nyan','unicorn'],
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-      'specs/client/routingSpec.js:': [ 'browserify']
-    },
-
+    // preprocessors: {
+    //   'specs/client/routingSpec.js:': [ 'browserify']
+    // },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
-
 
     // web server port
-    port: 9876,
-
+    // port: 9876,
 
     // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
+    // colors: true,
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+    // logLevel: config.LOG_INFO,
 
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', 'PhantomJS'],
+    browsers: ['PhantomJS'],
 
-    // Continuous Integration mode
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
+    singleRun: true
 
-  })
-}
+  });
+};
