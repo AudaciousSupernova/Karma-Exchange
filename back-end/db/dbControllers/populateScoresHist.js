02be10532e03c0b5b13a5067a48c294dd8bf60b8
Kartik <kvempati11@gmail.com>
2016-01-30 12:34:37 -0800

    Add randomDate function to populate scoresHist with randomDates

--- back-end/db/dbControllers/populateScoresHist.js
+++ back-end/db/dbControllers/populateScoresHist.js
@@ -8,17 +8,49 @@ mainController.getAllUsers(function(err, users){
 	var totalAdditions = 0;
 	var currentAddition = 1;
 
+	var generateDate = function () {
+		// '0000-00-00 00:00:00' MySQL timestamp format
+		var year = (Math.floor(Math.random() * (2017 - 2015)) + 2015).toString();
+		if (year === "2015") {
+			var month = (Math.floor(Math.random() * (13 - 11)) + 11).toString();
+		}
+		else {
+			var month = (Math.floor(Math.random() * (3 - 1)) + 1).toString();
+		}
+		var day = (Math.floor(Math.random() * (29 - 1)) + 1).toString();
+		var hour = (Math.floor(Math.random() * (24 - 0)) + 0).toString();
+		var minute = (Math.floor(Math.random() * (60 - 0)) + 0).toString();
+		var second = (Math.floor(Math.random() * (60 - 0)) + 0).toString();
+		if (hour.length < 2){
+			hour = '0'+ hour;
+		}
+		if (minute.length < 2){
+			minute = '0'+ minute;
+		}
+		if (second.length < 2){
+			second = '0'+ second;
+		}
+		if (day.length < 2){
+			day = '0'+ day;
+		}
+		if (month.length < 2){
+			month = '0'+ month;
+		}
+		return year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second;
+	}
+
 	for(var i = 0; i < length; i++){
 	// for(var i = 0; i < users.length; i++){
 		var numberOfNewScores = Math.floor(Math.random() * 20);
 		totalAdditions += numberOfNewScores;
-		
+
 		for(var subI = 0; subI < numberOfNewScores; subI++){
 			var randomScoreObj = {
 				user_id: i+1,
 				social: Math.ceil(Math.random() * 100),
 				social_investment: Math.ceil(Math.random() * 100),
 				currentScore: Math.ceil(Math.random() * 100),
+				ts: generateDate()
 			}
 			mainController.addScore(randomScoreObj, function(err, response){
 				console.log("inserting score # " + currentAddition)
@@ -28,6 +60,6 @@ mainController.getAllUsers(function(err, users){
 				}
 				currentAddition++;
 			})
-		}		
+		}
 	}
 })
