791063f2e47a123284d40b3f4de5c73daa72d7b4
Neeraj Kohirkar <neerajk1208@gmail.com>
2016-01-30 11:23:10 -0800

    no longer calling getscore and instead grabbing the current score from finding the user

--- front-end/app/controllers/portfolio.js
+++ front-end/app/controllers/portfolio.js
@@ -34,7 +34,7 @@ angular.module('app.portfolio', [])
       })
   }
 
-  function SellModalController($scope, $mdDialog, investment, loggedinUserInfo, TransactionHist, Scores) {
+  function SellModalController($scope, $mdDialog, investment, loggedinUserInfo, TransactionHist, Scores, User) {
 
 
     $scope.investment = investment;
@@ -45,39 +45,27 @@ angular.module('app.portfolio', [])
     console.log("this is the investment", $scope.investment)
     // console.log("this is shares to sell", $scope.sharesToSell);
 
-  $scope.getScores = function () {
+  $scope.getUserById = function () {
     // console.log("what does my user contain", $scope.investment.target_id);
-    Scores.getScores($scope.investment.target_id)
+    User.getUser($scope.investment.target_id)
       .then(function (results) {
-        // console.log('I AM HERE');
-        // console.log(results, "Scores from Score factory");
-        if (results.length === 0) {
-          $scope.scores = [];
-        } else {
-          $scope.scores = results;
-          // console.log("what exactly is scope.scores?", $scope.scores);
-          $scope.targetCurrentScore = $scope.scores[0].social.total;
-          // console.log("do we have it here?", $scope.targetCurrentScore)
-          var transaction = {
-            user_id: $scope.investment.user_id,
-            target_id: $scope.investment.target_id,
-            type: "sell",
-            numberShares: $scope.sharesToSell,
-            karma: $scope.sharesToSell * $scope.targetCurrentScore//reference logged in user's karma
-          }
 
-        // console.log("WHAT IS MY CURRENT SCORE", $scope.targetCurrentScore);
-        // console.log($scope.scores, 'scores in the sell controller');
-          // console.log("is the current score being saved", $scope.targetCurrentScore)
-          $scope.loggedinUserInfo.karma = $scope.loggedinUserInfo.karma + $scope.sharesToSell * $scope.targetCurrentScore;
-          TransactionHist.addTransaction(transaction)
-            .then(function(results) {
-              investment.numberShares = investment.numberShares - $scope.sharesToSell;
-              $mdDialog.hide();
-
-            })
+        var transaction = {
+          user_id: $scope.investment.user_id,
+          target_id: $scope.investment.target_id,
+          type: "sell",
+          numberShares: $scope.sharesToSell,
+          karma: $scope.sharesToSell * results[0].currentScore//reference logged in user's karma
         }
 
+        
+        $scope.loggedinUserInfo.karma = $scope.loggedinUserInfo.karma + $scope.sharesToSell * results[0].currentScore;
+        TransactionHist.addTransaction(transaction)
+          .then(function(results) {
+            investment.numberShares = investment.numberShares - $scope.sharesToSell;
+            $mdDialog.hide();
+
+          })
       })
   }
 
@@ -86,7 +74,7 @@ angular.module('app.portfolio', [])
         console.log("You are trying to sell more than you have!");
         $mdDialog.hide();
       } else {
-        $scope.getScores()
+        $scope.getUserById()
 
         //grab the target_id
         //get scores by target_id
