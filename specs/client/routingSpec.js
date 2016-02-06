1269d032821411c4ec3e659caa3cbee03c5a029e
Ranjit Rao <ranjiterao@gmail.com>
2016-02-06 11:53:50 -0800

    routingSpec.js complete

--- specs/client/routingSpec.js
+++ specs/client/routingSpec.js
@@ -1,52 +1,5 @@
-// describe('Routing', function() {
-// 	var $state;
-// 	beforeEach(module('app'));
-
-// 	beforeEach(inject(function ($injector, $state) {
-// 		$state = $injector.get('$state');
-// 		console.log('$here i am!!!!!');
-// 		console.log('$state', $state.url); 
-// 	}));
-	
-//   it('Should have home route, template, and controller', function () {
-// 	  expect(true).to.equal(true); 
-// 	});
-
-// });
-
-// re-comment this back most likely! 
-// describe('Routing', function() {
-
-// 	var $rootScope, $state, $injector, myServiceMock, state = 'myState';
-
-// 	beforeEach(function() {
-
-// 		module('app', function($provide) {
-//       $provide.value('myService', myServiceMock = {});
-//     });
-
-//     inject(function(_$rootScope_, _$state_, _$injector_, $templateCache) {
-//       $rootScope = _$rootScope_;
-//       $state = _$state_;
-//       $injector = _$injector_;
-//       console.log('THIS IS $ROOTSCOPE:', $rootScope);
-//       console.log('THIS IS $STATE:', $state);
-//       console.log('THIS IS $INJECTOR:', $injector); 
-//       // We need add the template entry into the templateCache if we ever
-//       // specify a templateUrl
-//       $templateCache.put('template.html', '');
-//       console.log('THIS IS $TEMPLATECACHE:', $templateCache); 
-//     })
-//   });
-
-// 	it('Should have home route, template, and controller', function () {
-// 	  expect(true).to.equal(true); 
-// 	});
-
-// });
-// end of re-comment section 
-
 describe('Routing', function() {
+
   var homeState, newsfeedState, profileState, buyState, sellState, portfolioState, transactionHistState;
 
   beforeEach(module('app')); 
@@ -128,44 +81,5 @@ describe('Routing', function() {
     expect(transactionHistState.url).to.equal('/portfolio/:id');
     expect(transactionHistState.templateUrl).to.equal('app/views/portfolio.html'); 
   });
-
+  
 }); 
-
-
-
-// describe('myApp/myState', function() {
-
-//   var $rootScope, $state, $injector, myServiceMock, state = 'myState';
-
-//   beforeEach(function() {
-
-//     module('myApp', function($provide) {
-//       $provide.value('myService', myServiceMock = {});
-//     });
-
-//     inject(function(_$rootScope_, _$state_, _$injector_, $templateCache) {
-//       $rootScope = _$rootScope_;
-//       $state = _$state_;
-//       $injector = _$injector_;
-
-//       // We need add the template entry into the templateCache if we ever
-//       // specify a templateUrl
-//       $templateCache.put('template.html', '');
-//     })
-//   });
-
-//   it('should respond to URL', function() {
-//     expect($state.href(state, { id: 1 })).toEqual('#/state/1');
-//   });
-
-//   it('should resolve data', function() {
-//     myServiceMock.findAll = jasmine.createSpy('findAll').andReturn('findAll');
-
-//     $state.go(state);
-//     $rootScope.$digest();
-//     expect($state.current.name).toBe(state);
-
-//     // Call invoke to inject dependencies and run function
-//     expect($injector.invoke($state.current.resolve.data)).toBe('findAll');
-//   });
-// });
