306828f2c3a7c1aa1ebfbca8f44117c72f41d83c
Ranjit Rao <ranjiterao@gmail.com>
2016-02-09 11:13:19 -0800

    servicesSpec.js - Auth factory test

--- specs/client/servicesSpec.js
+++ specs/client/servicesSpec.js
@@ -30,7 +30,7 @@ describe('Services', function() {
   		var mockResponse = [{
   			name: "TomDickHarry",
   	    password: "oogabooga",
-  	    email: "ranjiterao@gmail.com",
+  	    email: "xyz@gmail.com",
   	    karma: 10,
   		  facebookKey: "1234",
   		  profile_photo: "blah",
@@ -79,18 +79,66 @@ describe('Services', function() {
       expect(Portfolio.addInvestment).to.be.a('function'); 
     })
 
-    it('should get a user investment portfolio with `getInvestments`', function() {
+    it('should get a user investment with `getInvestments`', function() {
       var mockResponse = [{
         user_id: 4,
         target_id: 2,
         numberShares: 10
       }];
-      
+
       $httpBackend.expect('GET', '/portfolio/4').respond(mockResponse);
 
       Portfolio.getInvestments(4).then(function(investment) {
         console.log('investment:', investment); 
         expect(investment[0].user_id).to.equal(4); 
+      });
+
+      $httpBackend.flush(); 
+    });
+
+  });
+
+  describe('Auth Factory', function() {
+    var $httpBackend, Auth; 
+
+    beforeEach(inject(function (_$httpBackend_, _Auth_) {
+      $httpBackend = _$httpBackend_;
+      Auth = _Auth_; 
+    }));
+
+    it('should exist', function() {
+      expect(Auth).to.exist;
+    });
+
+    it('should have a method `checkLoggedIn`', function() {
+      expect(Auth.checkLoggedIn).to.be.a('function');
+    });
+
+    it('should check if a user is logged in with `checkLoggedIn`', function() {
+      var mockResponse = [{
+        name: "TomDickHarry",
+        password: "oogabooga",
+        email: "xyz@gmail.com",
+        karma: 10,
+        facebookKey: "1234",
+        profile_photo: "blah",
+        social: 5,
+        social_subScores: "{friendScore:5,photoScore:5,feedScore:5}",
+        social_investment_subScores: "{numShareHolder:0,sharesOnMarket:0,numTransactions:0}",
+        last_week_expected_social_change: '0%',
+        last_week_actual_social_change: '0%',
+        next_week_expected_social_change: '0%',
+        social_investment: 5,
+        currentScore: 10,
+        id: 2,
+        access_token: "accesstoken"
+      }];
+
+      $httpBackend.expect('GET', '/api/loggedin').respond(mockResponse);
+
+      Auth.checkLoggedIn().then(function(user) {
+        console.log('user:', user); 
+        expect(user).to.equal(true); 
       });
 
       $httpBackend.flush(); 
